60f34ce0161675c0555cf71ac5c75bcb66c7790c
Fillsogood <94848819+Fillsogood@users.noreply.github.com>
2022-04-11 19:23:37 +0900

    sd

---
+++ Assets/scripts/DefectIni.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefectIni : MonoBehaviour
+{     
+    List<float> x = new List<float>();
+    List<float> y = new List<float>();
+    List<float> z = new List<float>();
+    Vector3 DefectPosition;
+    void Start()
+    {
+        x.Add(19.5f);
+        x.Add(20.822f);
+        y.Add(-1f);
+        y.Add(-1f);
+        z.Add(9.2f);
+        z.Add(11.74f);
+
+        for(int i =0;i<x.Count;i++)
+        {
+            DefectPosition = new Vector3(x[i],y[i],z[i]);
+            Quaternion a = new Quaternion(0,0,0,0);
+            GameObject Defectpoint= Resources.Load<GameObject>("DefectPrefab/Defect");  
+            GameObject Instance = (GameObject) Instantiate(Defectpoint, DefectPosition,a);
+            
+            
+        }
+        
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/FollowUser.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//script to make secondary camera follow user.
+public class FollowUser : MonoBehaviour
+{  
+    GameObject Target; // Capsule 게임오브젝트 가져오기
+    public Transform TargetCapsule;
+    public Transform Navigation;
+
+    void Update()
+    {
+        if(TargetCapsule==null)
+        {
+          Target = GameObject.Find("Capsule");
+
+          TargetCapsule = Target.transform;
+        }
+        else
+        {
+          
+        }
+    }
+    private void LateUpdate()
+    {
+      Navigation.position =new Vector3(TargetCapsule.position.x,8.36f,TargetCapsule.position.z);
+      Navigation.LookAt(TargetCapsule);
+    }
+
+}
